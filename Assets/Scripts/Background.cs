e49839ddf2690b26cfd937a9dd8c74334d236b5e
Kako Botasso <kakodev@gmail.com>
2017-09-01 21:48:32 -0300

    Cria movimento da pista

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public float yLimit;
+	public float yReturn;
+	public float velocity;
+
+	void Update () {
+		transform.Translate (Vector2.down * velocity * Time.deltaTime);
+
+		if (transform.position.y < yLimit) {
+			transform.position = new Vector3 (0.0f, yReturn, 0.0f);	
+		} 
+	}
+}
