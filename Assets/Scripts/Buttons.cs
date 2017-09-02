7a812374a1b7312c173909318413d8173f1a3724
Kako Botasso <kakodev@gmail.com>
2017-09-01 21:23:16 -0300

    Cria tela de menu

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public void GoToGame(){
+		SceneManager.LoadScene ("game");
+	}
+}
