f6f347e777b3a4669e94e173ba763023a528e8f7
Kako Botasso <kakodev@gmail.com>
2017-09-04 01:29:34 -0300

    Cria tela de jogo

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,10 +1,30 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour {
 
-	public void GoToGame(){
+	public Button mainMenu;
+	public Button playAgain;
+
+	void Start () {
+		Time.timeScale = 1;
+		if (playAgain != null) {
+			playAgain.onClick.AddListener (GoToGame);
+		}
+
+		if (mainMenu != null) {
+			mainMenu.onClick.AddListener (GoToMenu);
+		}
+			
+	}
+
+	public void GoToGame () {
 		SceneManager.LoadScene ("game");
+	}
+
+	public void GoToMenu () {
+		SceneManager.LoadScene ("menu");
 	}
 }
---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	public float velocity;
+	GameObject[] gameOverObjects;
+
+	void Start () {
+		gameOverObjects = GameObject.FindGameObjectsWithTag ("gameOver");
+
+		foreach (GameObject g in gameOverObjects) {
+			g.SetActive (false);
+		}
+	}
+
+	void Update () {
+		Vector3 dir = new Vector3(Input.acceleration.x * velocity, 0.0f, 0.0f);
+		transform.position += dir;	
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		foreach (GameObject g in gameOverObjects) {
+			g.SetActive (true);
+		}
+		Time.timeScale = 0;
+	}
+
+}
