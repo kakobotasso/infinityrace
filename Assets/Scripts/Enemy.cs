1e2a506f07a7001df83c5c6eee362be7b378e2ef
Kako Botasso <kakodev@gmail.com>
2017-09-06 21:03:19 -0300

    Gera inimigos aleatoriamente

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,5 +9,17 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector2.down * velocity * Time.deltaTime);
+
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+		if (screenPosition.x <= -5.0f){
+			Destroy (gameObject);
+		}
+
+		if (screenPosition.y <= -Screen.height){
+			// UPDATE SCORE
+			// INCREMENT VELOCITY
+			Destroy (gameObject);
+		}
 	}
 }
---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyGenerator : MonoBehaviour {
+
+	public GameObject enemy;
+	public float line01;
+	public float line02;
+	public float line03;
+	public float line04;
+
+	bool line01Used = false;
+	bool line02Used = false;
+	bool line03Used = false;
+	bool line04Used = false;
+
+	bool running = true;
+	string allRandom = "";
+
+	void Start () {
+		InvokeRepeating ("CreateEnemy", 0.5f, 3.5f);
+	}
+
+	void CreateEnemy() {
+		if (running) {
+			int numberEnemies = UnityEngine.Random.Range (1, 4);
+
+			for (int i = 0; i < numberEnemies; i++) {
+				enemy.transform.position = new Vector3(RandomLine(), 7.0f, 0.0f) ;
+				Instantiate (enemy);
+			}
+
+		}
+
+		line01Used = false;
+		line02Used = false;
+		line03Used = false;
+		line04Used = false;
+	}
+
+	float RandomLine(){
+		int actualRandom = UnityEngine.Random.Range (0, 5);
+
+		switch (actualRandom) {
+		case 1:
+			if (!line01Used) {
+				line01Used = true;
+				return line01;
+			}
+			break;
+		case 2:
+			if (!line02Used) {
+				line02Used = true;
+				return line02;
+			}
+			break;
+		case 3:
+			if (!line03Used) {
+				line03Used = true;
+				return line03;
+			}
+			break;
+		case 4:
+			if (!line04Used) {
+				line04Used = true;
+				return line04;
+			}
+			break;
+		}
+
+		return -5.0f;
+	}
+
+}
